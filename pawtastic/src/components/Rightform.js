b78d573fc75df64b24a02c3aeac0bbe953eb9627
Visa <visalakshi.cse@gmail.com>
2020-04-02 01:13:04 -0500

    Added select event

--- pawtastic/src/components/Rightform.js
+++ pawtastic/src/components/Rightform.js
@@ -13,7 +13,10 @@ export default class Rightform extends Component {
     Spay:'',
   }
 
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleGenderClick=this.handleGenderClick.bind(this);
+    this.handleSpayedClick=this.handleSpayedClick.bind(this);
+    this.handleWeightOptionClick=this.handleWeightOptionClick.bind(this);
     
   }
 
@@ -21,70 +24,72 @@ export default class Rightform extends Component {
     this.setState({[event.target.name]: event.target.value})
     // this.setState({Breed: event.target.Breed});
   }
+  handleGenderClick(event) {
+    this.setState({Gender: event.target.value});
+    ;
+}
+handleSpayedClick(event) {
+    this.setState({Spayed: event.target.value});
+    
+}
+handleWeightOptionClick(event) {
+    this.setState({Weight: event.target.value});
+    alert(event.target.value);
+    
+}
 
-  
-
-  render() {
+render() {
     return (
       <div id="header">
       <form id="form1" onSubmit={this.handleChange}>
         <h1>Yay We Love Dogs!,Give us the basic about your pup</h1>
         <label>  
-          Name: 
+        Name: 
           <br />
           <input name="name1" type="text"  value={this.state.name1} onChange={this.handleChange}/>
           </label>
           <label>
-              
           <input name="name1" type="text"  value={this.state.name1} onChange={this.handleChange}/>
-       
           </label>
-         
           <br />
           <label> 
           Breed: 
-         <br />
+          <br />
           <input name="Breed" type="text"  value={this.state.Breed} onChange={this.handleChange}/>
           </label>
-  
           <label> 
           Birthday:
           <br />
           <input name="Bday"type="text"  value={this.state.Bday} onChange={this.handleChange}/> 
           </label>
-          
-          
           <br />
           <label>
-        
-        Gender 
-                <br />
-                <select className='twoOptions' size="2">
-                <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                  
-                </select>
-              </label>
+          Gender 
+          <br />
+          <select className='twoOptions' size="2" onClick={this.handleGenderClick}>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          </select>
+          </label>
           <label> 
           Spayed
           <br />
-          <select className='twoOptions' size="2">
-                <option value="Yes">Yes</option>
-                  <option value="No">No</option>
-                  
-                </select>
+          <select className='twoOptions' size="2" onClick={this.handleSpayedClick}>
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+          </select>
           </label>
           <br />
           
           <label> 
           Weight
           <br />
-          <select className='twoOptions' size="2">
-                <option value="Male">0-25lbs</option>
-                  <option value="Male">25-50lbs</option>
-                  <option value="Male">50-100lbs</option>
-                  <option value="Male">100+ lbs</option>
-                </select>
+          <select className='fourOptions' size="2" onClick={this.handleWeightOptionClick}>
+                <option value="25">0-25lbs</option>
+                  <option value="50">25-50lbs</option>
+                  <option value="100">50-100lbs</option>
+                  <option value="100+">100+ lbs</option>
+          </select>
           </label>
           
  <br/>
